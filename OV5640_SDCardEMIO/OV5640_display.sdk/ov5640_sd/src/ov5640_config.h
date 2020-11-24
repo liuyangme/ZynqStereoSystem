00a5207612847a40a03ecb84be15774ba2a8b5c9
Liu Yang <ly_0913@foxmail.com>
2020-11-24 21:02:04 +0800

    update

---
+++ OV5640_SDCardEMIO/OV5640_display.sdk/ov5640_sd/src/ov5640_config.h
@@ -0,0 +1,13 @@
+/*
+ * ov5640_config.h
+ *
+ *  Created on: 2016Äê10ÔÂ23ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef OV5640_CONFIG_H_
+#define OV5640_CONFIG_H_
+
+void ov5640_init_rgb(void);
+
+#endif /* OV5640_CONFIG_H_ */
